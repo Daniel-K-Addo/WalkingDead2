98878b1092a0aa915fd578a49a317e5cd9de0245
Daniel Kevin Addo <danieladdo99@gmail.com>
2023-03-15 04:13:50 -0600

    Batch of updates

--- R/wd2Function.R
+++ R/wd2Function.R
@@ -1,47 +1,61 @@
 #' @title Walking Dead 2
-#' @description This package simulates the events of infection and healing
-#' in a population of susceptible individuals, infected individuals, and healed
-#' individuals. The function is only at the development stage and thus has a
+#' @description This package simulates the events of infection (of susceptible subjects)
+#' and immunization (of infected subjects) in a population of susceptible, infected,
+#' and immunized individuals. The function is still at the development stage and thus has a
 #' limited number of arguments that can be altered for the simulation process.
 #' @param n,m,k Numeric scalars. \code{n} represents the total population size;
 #' \code{m} represents the infected population size; \code{k} represents the
 #' healing population size.
+#' @param fixedAllocation logical with default set to \code{FALSE}. When \code{FALSE},
+#' group sizes are randomly determined using probability given \code{{n,m,k}}.
+#' When \code{TRUE}, groups sizes will be exactly as stated given \code{{n,m,k}}.
 #' @param infect numeric value between 0 and 1. This represents the probability
 #' of rate of change from one status to another on contact.
 #' @param infect_radius numeric value between 0 and 1. Represents the radius within which an
 #' individual can experience a status change when exposed
 #' @param nIterations positive integer. Represents the number of trials to run
 #' where each individual's status and position may be updated.
-#' @param flight logical with default set to TRUE. When TRUE, the infected are
-#' programmed to repel from the healers. When FALSE, the infected are programmed
+#' @param flight logical with default set to \code{TRUE}. When \code{TRUE}, the infected are
+#' programmed to repel from the healers. When \code{FALSE}, the infected are programmed
 #' to be attracted to the non-infected.
 #' infected.
 #' @examples
 #' require(data.table)
-#' require(grDevices)
 #' temp <- wd2(n=100, m=15, k=5)
 #' temp2 <- summary(temp)
 #' temp3 <- plot(temp)
 #' # head(temp2)
 #' # temp3
 #' @export
-wd2 <-  function(n,
+wd2 <-  function(n, # Population size
                  m, # infected group
-                 k, # curing group
+                 k, # curing group,
+                 fixedAllocation = FALSE, # allocation type
                  infect = .75, # infection rate
                  infect_radius = .075, # radius of infection
                  nIterations = 10, # Number of iterations
                  flight = TRUE # Do infected flee or not
                  ){
-  # Group individuals into three categories given respective probs
-  statusGrouping <- stats::rmultinom(n, size= 1, prob = c((1-m/n-k/n),m/n,k/n))
+  if(fixedAllocation){
+    statusGrouping <- c(rep(1,n-m-k), rep(2,m), rep(3,k))
+    statusGrouping <- sample(statusGrouping)
+  }else{
+    # Group individuals into three categories given respective probs
+    statusGrouping <- stats::rmultinom(n, size= 1, prob = c((1-m/n-k/n),m/n,k/n)) |>
+      apply(2, which.max)
+    if(any(statusGrouping!=3)){
+      statusGrouping[length(statusGrouping)] <- 3
+    }
+  }
+
+
 
   # Creating the baseline environment
   ans <- list2env(list(
     n      = n,
     pos    = matrix(runif(n*2), ncol=2),
     degree = stats::runif(n, 0, 2*pi),
-    status   = apply(statusGrouping, 2, which.max),
+    status   = statusGrouping,
     infect = infect,
     radii  = infect_radius,
     speed  = stats::runif(n, 0.005, .025)/2
@@ -146,20 +160,33 @@ wd2 <-  function(n,
   }
 
   # Plot of each individual's status and position at any time t
-  plot_process <- function(x) {
-
-    op <- graphics::par(mar=rep(1, 4), mai = rep(0, 4))
-    on.exit(par(op))
-
-    graphics::plot.new()
-
-    graphics::plot.window(c(0,1), c(0,1))
-    graphics::rect(0,0,1,1, col = "gray70", border = "transparent")
-    graphics::points(
-      x$pos,
-      bg = c("black", "red","green")[x$status],
-      pch = 21, col="white", cex=2
-    )
+  plot_process <-  function(ans) {
+    plotData <- data.frame(Status=factor(ans$status, labels = c("Susceptible","Infected","Immune")),
+                           posX=ans$pos[,1],
+                           posY=ans$pos[,2])
+    ggplot2::ggplot(plotData) +
+      ggplot2::geom_point(ggplot2::aes(x=posX,y=posY,col=Status), size=2.5) +
+      ggplot2::scale_color_manual(values = c("black", "firebrick","chartreuse3"),
+                                  labels = c(paste("Susceptible:", mean(ans$status==1)*ans$n),
+                                             paste("Infected:", mean(ans$status==2)*ans$n),
+                                             paste("Immune:", mean(ans$status==3)*ans$n))) +
+      ggplot2::xlim(0,1) + ggplot2::ylim(0,1) +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(# Remove vertical grid lines
+        panel.grid.major.x = ggplot2::element_blank(),
+        panel.grid.minor.x = ggplot2::element_blank(),
+        panel.grid.minor.y = ggplot2::element_blank(),
+        panel.grid.major.y = ggplot2::element_blank(),
+        axis.text.x=ggplot2::element_blank(),
+        axis.text.y=ggplot2::element_blank(),
+        axis.ticks.x=ggplot2::element_blank(),
+        axis.ticks.y=ggplot2::element_blank(),
+        axis.title.x=ggplot2::element_blank(),
+        axis.title.y=ggplot2::element_blank(),
+        legend.position="bottom",
+        legend.text = ggplot2::element_text(size = 15),
+        plot.title = ggplot2::element_text(hjust = 0.5),
+        panel.background=ggplot2::element_rect(colour="white", fill = "grey90"))
   }
   # ------------------------------------------------------------------------------
 
@@ -169,21 +196,20 @@ wd2 <-  function(n,
 
   # Setting animation
   grDevices::graphics.off()
-  fig <- magick::image_device(300, 300)
+  fig <- magick::image_graph(600, 600)
 
   for (i in 1:nIterations) {
 
     update_status(ans)
     snapshot[i+1,] <- ans$status
-    plot_process(ans)
+    p <- plot_process(ans)
+    print(p)
     update_pos(ans)
   }
-  while (!is.null(grDevices::dev.list()))
-  grDevices::dev.off()
 
   structure(list(wd2Summary = snapshot,
                  wd2Plot = fig,
-                 n=n),
+                 wd2Object = ans),
             class = "wd2")
 }
 #' @rdname wd2
@@ -200,7 +226,7 @@ plot.wd2 <- function(x,...){
 #' @param ... additional arguments affecting the summary produced.
 summary.wd2 <- function(object,...){
   snapshot <- object[["wd2Summary"]]
-  n <- object[["n"]]
+  n <- object[["wd2Object"]][["n"]]
   nIteration <- dim(snapshot)[1]
   state <- matrix(NA, ncol = 3, nrow = nIteration)
   for(i in 1:nIteration){
@@ -210,11 +236,13 @@ summary.wd2 <- function(object,...){
     )
   }
   stateDT <- data.table::setDT(as.data.frame(state))[]
-  data.table::setnames(stateDT,1:3,c('Non_Infected','Infected','Healed'))
-  stateDT[,run:=1:nrow(stateDT)]
-  stateDT[,changeNI:= (Non_Infected - c(NA,Non_Infected[-.N]))*n]
-  stateDT[,changeIH:= (Infected-- c(NA,Infected[-.N]))*n]
-  stateDT[,transitions:= changeNI+changeIH]
+  data.table::setnames(stateDT,1:3,c('Susceptible','Infected','Immune'))
+  stateDT[,Run:=1:nrow(stateDT)]
+  stateDT[,ChangeSI:= (Susceptible - c(NA,Susceptible[-.N]))*n]
+  stateDT[,ChangeII:= (Infected-- c(NA,Infected[-.N]))*n]
+  stateDT[,Transitions:= ChangeSI+ChangeII]
+  setcolorder(stateDT, c("Run",'Susceptible','Infected','Immune',
+                         "ChangeSI","ChangeII","Transitions"))
   return(stateDT)
 }
 
