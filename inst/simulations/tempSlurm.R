9fd9b02ad50940a64454a1bacdd46c69385771da
Daniel Kevin Addo <danieladdo99@gmail.com>
2023-05-04 16:51:44 -0600

    Update to slurm files

--- inst/simulations/tempSlurm.R
+++ inst/simulations/tempSlurm.R
@@ -1,7 +1,9 @@
 require(dplyr)
+require(WalkingDead2)
+require(slurmR)
 
-NN <- seq(50,500,length=4) # Sample size options
-RR <- seq(0.2,0.8, length=3) # Infection Rate options
+NN <- seq(50, 200, length=3) # Sample size options
+RR <- seq(0.25, 0.75, length=3) # Infection Rate options
 FF <- c(TRUE, FALSE) # Flight or fight
 NNRRFF <- expand.grid(NN,RR,FF) # Combinations of NN and RR
 NNRRFFrep <- NNRRFF %>% slice(rep(1:n(), each=500))
@@ -18,22 +20,28 @@ simwd2 <- function(j){
                  infect_radius = .075, # radius of infection
                  nIterations = 50, # Number of runs
                  flight = FF # Infected will flee from immunized if TRUE
-                 )
+  )
   keepThis <- snapshot(testRun)
   return(keepThis)
 }
 
-require(slurmR)
-require(WalkingDead2)
-
-cl <- makeSlurmCluster(16, partition = "kingspeak", account = "phs7045")
-# require(parallel)
-# cl <- parallel::makeCluster(6)
-clusterExport(cl,c("NNRRFF","wd2","simwd2","snapshot"))
+require(parallel)
+cl <- parallel::makeCluster(16)
+clusterExport(cl,c("NNRRFFrep", "simwd2"))#,"wd2","simwd2","snapshot"))
+clusterEvalQ(cl, library(WalkingDead2))
 temp <- do.call(rbind.data.frame,parLapply(cl,1:dim(NNRRFFrep)[1],simwd2))
 stopCluster(cl)
 saveRDS(temp, "SimWD2Out.rds")
-
+#
+# cl <- makeSlurmCluster(16, partition = "notchpeak-shared-short",
+#                        account = "notchpeak-shared-short")
+#
+# clusterExport(cl,c("NNRRFFrep", "simwd2"))#,"wd2","simwd2","snapshot"))
+# clusterEvalQ(cl, library(WalkingDead2))
+# temp <- do.call(rbind.data.frame,parLapply(cl,1:16,simwd2))
+# stopCluster(cl)
+# saveRDS(temp, "SimWD2Out.rds")
+#
 # njobs <- 10L
 # temp <- do.call(rbind.data.frame,
 #                 Slurm_sapply(1:dim(NNRRFF)[1],
@@ -41,8 +49,9 @@ saveRDS(temp, "SimWD2Out.rds")
 #                              njobs    = njobs,
 #                              plan     = "collect",
 #                              tmp_path = "/scratch/general/nfs1/u1372213", # This is where all temp files will be exported
-#                              sbatch_opt = list(account = "phs7045",
-#                                                 partition = "kingspeak-shared")
+#                              sbatch_opt = list(account = "notchpeak-shared-short",
+#                                                 partition = "notchpeak-shared-short"),
+#
 #                              )
 #                 )
 #
